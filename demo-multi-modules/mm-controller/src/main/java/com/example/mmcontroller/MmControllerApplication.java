ab11a62a70eac318d0bea07823dce34ebe2509f6
ttyrion <tyrion.marting@hotmail.com>
2020-07-28 15:38:47 +0800

    初始的多模块Spring Boot项目

---
+++ demo-multi-modules/mm-controller/src/main/java/com/example/mmcontroller/MmControllerApplication.java
@@ -0,0 +1,13 @@
+package com.example.mmcontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmControllerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MmControllerApplication.class, args);
+    }
+
+}
---
+++ demo-multi-modules/mm-domain/src/test/java/com/example/mmdomain/MmDomainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mmdomain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MmDomainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
