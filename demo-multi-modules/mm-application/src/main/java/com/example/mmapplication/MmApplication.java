14e676dbe2fcaffd35b61ee5f12a706c99a01130
ttyrion <tyrion.marting@hotmail.com>
2020-08-28 11:37:33 +0800

    @Conditional and @SpringBootTest

--- demo-multi-modules/mm-application/src/main/java/com/example/mmapplication/MmApplication.java
+++ demo-multi-modules/mm-application/src/main/java/com/example/mmapplication/MmApplication.java
@@ -1,8 +1,12 @@
 package com.example.mmapplication;
 
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.web.context.WebServerApplicationContext;
+import org.springframework.context.annotation.*;
+import com.example.mmapplication.conditions.RunnerCondition;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.example")
@@ -11,4 +15,16 @@ public class MmApplication {
         System.out.println("service is starting...");
         SpringApplication.run(MmApplication.class, args);
     }
+
+    @Bean
+    @Conditional(RunnerCondition.class)
+    public ApplicationRunner runner(WebServerApplicationContext context) {
+        return args -> {
+            System.out.println("*************");
+            System.out.println("Current Web Server is :"
+                    + context.getWebServer().getClass().getName());
+            System.out.println("*************");
+        };
+    }
 }
+
--- demo-multi-modules/mm-application/src/test/java/com/example/mmapplication/MmApplicationTests.java
+++ demo-multi-modules/mm-application/src/test/java/com/example/mmapplication/MmApplicationTests.java
@@ -3,7 +3,13 @@ package com.example.mmapplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+/**
+ * https://docs.spring.io/spring-boot/docs/current/reference/html/spring-boot-features.html#boot-features-testing
+ * Package Error: No qualifying bean of type 'org.springframework.boot.web.context.WebServerApplicationContext' available
+ * Reason：By default, @SpringBootTest will not start a server.
+ * But: MmApplication启动时需要创建的ApplicationRunner bean依赖了WebServerApplicationContext bean
+* */
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class MmApplicationTests {
 
     @Test
