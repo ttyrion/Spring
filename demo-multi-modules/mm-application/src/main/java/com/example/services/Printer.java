a61d06438fad1f4defe287ee7777ae73ca1364ec
ttyrion <tyrion.marting@hotmail.com>
2020-08-31 17:03:50 +0800

    Async 与 this

--- demo-multi-modules/mm-application/src/main/java/com/example/services/Printer.java
+++ demo-multi-modules/mm-application/src/main/java/com/example/services/Printer.java
@@ -11,6 +11,7 @@ public class Printer {
         System.out.print(s);
     }
 
+    //Async 在通过this调用此方法时，异步无效：https://my.oschina.net/guangshan/blog/1807721
     @Async
     public void AsyncPrint(String s) throws InterruptedException {
         System.out.print("On thread:" + Thread.currentThread().getName() + "\n");
